8c25a2ac31b777df87fd75e06137a637f949d312
hoang.nguyen3 <hoang.nguyen3@amela.vn>
2021-11-17 16:00:52 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,141 @@
+import 'package:drag_and_drop_lists/drag_and_drop_lists.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: DragAndDrop()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class DragAndDrop extends StatefulWidget {
+  const DragAndDrop({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => DragAndDropState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class DragAndDropState extends State<DragAndDrop> {
+  List<DragAndDropList> list = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    list.add(buildBoard("Task", buildTasks(3)));
+    list.add(buildBoard("Doing", buildTasks(4)));
+    list.add(buildBoard("Done", buildTasks(7)));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Demo Drag and Drop"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: PageView.builder(
+        scrollDirection: Axis.horizontal,
+        controller: PageController(),
+        itemBuilder: (context, itemIndex) => DragAndDropLists(
+          axis: Axis.horizontal,
+          listPadding: const EdgeInsets.all(5.0),
+          listWidth: 200,
+          listDraggingWidth: 200,
+          onListReorder: (int oldListIndex, int newListIndex) {
+            setState(() {
+              var movedList = list.removeAt(oldListIndex);
+              list.insert(newListIndex, movedList);
+            });
+          },
+          onItemReorder: (int oldItemIndex, int oldListIndex, int newItemIndex,
+              int newListIndex) {
+            setState(() {
+              var movedItem =
+                  list[oldListIndex].children.removeAt(oldItemIndex);
+              list[newListIndex].children.insert(newItemIndex, movedItem);
+            });
+          },
+          children: list,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // body: DragAndDropLists(
+      //   axis: Axis.horizontal,
+      //   listPadding: const EdgeInsets.all(5.0),
+      //   listWidth: 200,
+      //   listDraggingWidth: 200,
+      //   onListReorder: (int oldListIndex, int newListIndex) {
+      //     setState(() {
+      //       var movedList = list.removeAt(oldListIndex);
+      //       list.insert(newListIndex, movedList);
+      //     });
+      //   },
+      //   onItemReorder: (int oldItemIndex, int oldListIndex, int newItemIndex,
+      //       int newListIndex) {
+      //     setState(() {
+      //       var movedItem = list[oldListIndex].children.removeAt(oldItemIndex);
+      //       list[newListIndex].children.insert(newItemIndex, movedItem);
+      //     });
+      //   },
+      //   children: list,
+      // ),
     );
   }
+
+  buildBoard(String name, List<DragAndDropItem> tasks) => DragAndDropList(
+        children: tasks,
+        header: Container(
+          width: 200,
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(7),
+              ),
+              color: Colors.redAccent),
+          padding: const EdgeInsets.all(20),
+          child: Text(
+            name,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+        ),
+        leftSide: const VerticalDivider(
+          color: Colors.redAccent,
+          width: 5,
+          thickness: 5,
+        ),
+        rightSide: const VerticalDivider(
+          color: Colors.redAccent,
+          width: 5,
+          thickness: 5,
+        ),
+        footer: Container(
+          color: Colors.redAccent,
+          height: 5,
+        ),
+        lastTarget: const SizedBox(
+          child: Center(
+            child: Icon(Icons.add),
+          ),
+          height: 15,
+        ),
+      );
+
+  buildItem(String item) => DragAndDropItem(
+        child: Card(
+          color: Colors.black12,
+          child: ListTile(
+            title: Text(
+              item,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ),
+      );
+
+  buildTasks(int length) => List<DragAndDropItem>.generate(
+        length,
+        (index) => buildItem('Task $index'),
+      );
 }
