8185508d26e9929a9ecff10253e4207b98bb0223
hoang.nguyen3 <hoang.nguyen3@amela.vn>
2021-11-17 17:38:55 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:drag_and_drop_lists/drag_and_drop_lists.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:snapping_page_scroll/snapping_page_scroll.dart';
 
 void main() {
   runApp(const MaterialApp(home: DragAndDrop()));
@@ -30,51 +31,27 @@ class DragAndDropState extends State<DragAndDrop> {
       appBar: AppBar(
         title: const Text("Demo Drag and Drop"),
       ),
-      body: PageView.builder(
-        scrollDirection: Axis.horizontal,
-        controller: PageController(),
-        itemBuilder: (context, itemIndex) => DragAndDropLists(
-          axis: Axis.horizontal,
-          listPadding: const EdgeInsets.all(5.0),
-          listWidth: 200,
-          listDraggingWidth: 200,
-          onListReorder: (int oldListIndex, int newListIndex) {
-            setState(() {
-              var movedList = list.removeAt(oldListIndex);
-              list.insert(newListIndex, movedList);
-            });
-          },
-          onItemReorder: (int oldItemIndex, int oldListIndex, int newItemIndex,
-              int newListIndex) {
-            setState(() {
-              var movedItem =
-                  list[oldListIndex].children.removeAt(oldItemIndex);
-              list[newListIndex].children.insert(newItemIndex, movedItem);
-            });
-          },
-          children: list,
-        ),
+      body: DragAndDropLists(
+        scrollController: ScrollController(),
+        axis: Axis.horizontal,
+        listPadding: const EdgeInsets.all(5.0),
+        listWidth: 200,
+        listDraggingWidth: 200,
+        onListReorder: (int oldListIndex, int newListIndex) {
+          setState(() {
+            var movedList = list.removeAt(oldListIndex);
+            list.insert(newListIndex, movedList);
+          });
+        },
+        onItemReorder: (int oldItemIndex, int oldListIndex, int newItemIndex,
+            int newListIndex) {
+          setState(() {
+            var movedItem = list[oldListIndex].children.removeAt(oldItemIndex);
+            list[newListIndex].children.insert(newItemIndex, movedItem);
+          });
+        },
+        children: list,
       ),
-      // body: DragAndDropLists(
-      //   axis: Axis.horizontal,
-      //   listPadding: const EdgeInsets.all(5.0),
-      //   listWidth: 200,
-      //   listDraggingWidth: 200,
-      //   onListReorder: (int oldListIndex, int newListIndex) {
-      //     setState(() {
-      //       var movedList = list.removeAt(oldListIndex);
-      //       list.insert(newListIndex, movedList);
-      //     });
-      //   },
-      //   onItemReorder: (int oldItemIndex, int oldListIndex, int newItemIndex,
-      //       int newListIndex) {
-      //     setState(() {
-      //       var movedItem = list[oldListIndex].children.removeAt(oldItemIndex);
-      //       list[newListIndex].children.insert(newItemIndex, movedItem);
-      //     });
-      //   },
-      //   children: list,
-      // ),
     );
   }
 
